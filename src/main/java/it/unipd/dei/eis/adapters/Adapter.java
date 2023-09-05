5f5e11fd2534fd5e692a238b6d2bfdd7fd84b1ad
MichaelOwusu20 <120094796+MichaelOwusu20@users.noreply.github.com>
2023-09-05 13:08:35 +0200

    Add files via upload

--- src/main/java/it/unipd/dei/eis/adapters/Adapter.java
+++ src/main/java/it/unipd/dei/eis/adapters/Adapter.java
@@ -3,6 +3,13 @@ package it.unipd.dei.eis.adapters;
 import it.unipd.dei.eis.Article;
 import java.util.ArrayList;
 
+/**
+ * La classe Adapter individua una cartella contenente diversi articoli
+ * di giornale e crea un'Arraylist di oggetti di tipo Article
+ * nei quali inserisce "Titolo" e "Contenuto" di ogni articolo.
+ * La classe è astratta quindi funziona per articoli
+ * che derivano da testate giornalistiche differenti.
+ */
 public abstract class Adapter {
 
    protected ArrayList<Article> articlesList = new ArrayList<>();
@@ -12,9 +19,24 @@ public abstract class Adapter {
       filePath = null;
    }
 
+   /**
+    * @return L'Arraylist contenente gli articoli nel formato Article.
+    */
    public ArrayList<Article> getArrayList() { return articlesList; }
+
+   /**
+    * @return Il percorso in cui si trova la cartella contenente gli articoli
+    * nei formati definiti dalle relative testate giornalistiche.
+    */
    public String getFilePath() { return filePath; }
+
+   /**
+    * @param path Nome del percorso in cui si trovano gli articoli.
+    */
    public void setFilePath(String path) { filePath = path; }
 
+   /**
+    * Carica nell'ArrayList gli articoli presenti nella cartella filePath.
+    */
    public abstract void loadArrayList();
 }
